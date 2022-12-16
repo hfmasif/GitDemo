cf4ffb6e76035008a5e4821d1641318a4e8b3697
Asif khan <43321908+hfmasif@users.noreply.github.com>
2022-12-17 00:11:48 +0600

    Update gitDemo.py

--- gitDemo.py
+++ gitDemo.py
@@ -5,3 +5,10 @@ print("Line 3 by Asif")
 print("Line 5 in Github")
 print("Line 6 in Local")
 print("Line 7 in Github")
+
+a = 10
+b = 5
+if a > b:
+  print("a bigger")
+else:
+  print("b is bigger")
